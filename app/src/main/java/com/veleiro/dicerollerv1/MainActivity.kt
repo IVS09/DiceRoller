a0a8d37e407b847f2c4dee7060b76515df3f9a96
mrlap <inveleiro@gmail.com>
2024-06-10 07:45:47 +0200

    findViewById, conect the button, OnClickListener, change the text

--- app/src/main/java/com/veleiro/dicerollerv1/MainActivity.kt
+++ app/src/main/java/com/veleiro/dicerollerv1/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.veleiro.dicerollerv1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val rollButton : Button = findViewById(R.id.roll_button)
+        rollButton.text = "Lets Roll"
+        rollButton.setOnClickListener {
+            rollDice()
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        val randomInt = Random.nextInt(6) + 1
+        resultText.text = randomInt.toString()
     }
 }
